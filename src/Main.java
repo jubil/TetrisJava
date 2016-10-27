0be36f9e31ae8231ae7e9d6b7d3563cbaa80b03c
jubil <valentin.53@orange.fr>
2016-10-28 01:01:26 +0200

    Création du Main.java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import gui.Fenetre;
+import modLoader.RessourceLoader;
+
+public class Main {
+
+	public static RessourceLoader rl = new RessourceLoader();
+	
+	public static void main(String[] args) {
+		
+		new Fenetre();
+		
+	}
+
+}
