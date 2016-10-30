0d03c850354b5a2efee504ae84a9343dcae8cbf7
jubil <valentin.53@orange.fr>
2016-10-30 23:40:07 +0100

    Changements mineurs sur la class Piece

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -1,11 +1,28 @@
 package model;
 
+import java.util.Random;
+
+import modLoader.BlockLoader;
+
 public class Piece {
 
 	private Block block;
 	int sens = 0;//de 0 à 3
 	int posX;
 	int posY;
+	
+	public Piece(){
+		this(BlockLoader.blocks.get(new Random().nextInt(BlockLoader.blocks.size())));
+	}
+	
+	public Piece(Block block){
+		this(block, new Random().nextInt(4));
+	}
+	
+	public Piece(Block block, int sens){
+		this.block = block;
+		this.sens = sens;
+	}
 	
 	public void sensSuivant(){
 		sens++;
