5382f314d92af467a8805deae70bc0b0923d14f0
jubil <valentin.53@orange.fr>
2016-11-02 14:44:21 +0100

    MapLoader

---
+++ src/modLoader/MapLoader.java
@@ -0,0 +1,34 @@
+package modLoader;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MapLoader {
+	
+	private static final String MAP_FOLDER = "/map/";
+
+	private BufferedImage[] maps;
+	
+	public MapLoader(){
+		System.out.println("RessourceLoader : START");
+		File dossierMap = new File(getClass().getClassLoader().getResource("map").getFile());
+		
+		maps = new BufferedImage[dossierMap.listFiles().length];
+		
+		for(int i = 0; i < dossierMap.listFiles().length; i++){
+			System.out.println("load "+dossierMap.listFiles()[i].getName());
+			try {
+				maps[i] = ImageIO.read(RessourceLoader.class.getResourceAsStream(MAP_FOLDER + dossierMap.listFiles()[i].getName()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		System.out.println("RessourceLoader : OK");
+	}
+	
+}
